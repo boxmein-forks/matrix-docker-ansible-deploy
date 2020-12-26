2740d3877b2c0da5dd62a32063d09138485b0480
boxmein <wsdf294@gmail.com>
2020-12-26 22:49:38 +0200

    vault is now in use

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ if ! command -v ansible-playbook >/dev/null 2>/dev/null; then
 	echo "please install ansible: pip install --user ansible"
 	exit 1
 fi
-ansible-playbook --ask-vault-pass -i inventory/hosts setup.yml --tags=setup-all
+ansible-playbook --ask-vault-pass -i inventory/hosts setup.yml --tags=start
 
