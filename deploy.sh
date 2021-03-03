8368a366ee753a603622a7bd47cc3ef92948f5db
boxmein <wsdf294@gmail.com>
2021-03-03 17:38:50 +0200

    update vaulted variables

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,34 @@ if ! command -v ansible-playbook >/dev/null 2>/dev/null; then
 	echo "please install ansible: pip install --user ansible"
 	exit 1
 fi
-ansible-playbook --ask-vault-pass -i inventory/hosts setup.yml --tags=setup-all -K
+
+run_ansible_command() {
+	ansible-playbook --ask-vault-pass -i inventory/hosts setup.yml --tags=${1}
+}
+
+verify_setup() {
+	printf "\x1b[31;1mMaking sure setup tasks are done\x1b[0m"
+	run_ansible_command setup-all
+}
+
+start() {
+	printf "\x1b[32;1mStarting services\x1b[0m"
+	run_ansible_command start
+}
+
+self_check() {
+	printf "\x1b[32;1mSelf-checking\x1b[0m"
+	run_ansible_command self-check
+}
+
+usage() {
+    echo "./deploy.sh (self-check|start|verify-setup)"
+}
+
+case $1 in
+	self-check) self_check ;;
+	start) start ;;
+	verify-setup) verify_setup ;;
+	*) usage ;;
+esac
 
